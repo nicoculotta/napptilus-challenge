18e47bbe07d4f2438e78d75249fb87c0d33bc4e1
nicoculotta <nicoculotta@gmail.com>
2022-03-26 17:21:26 -0300

    final details

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,8 +1,8 @@
 import React, { useContext } from 'react';
 import { useParams } from 'react-router';
-import { Container, Flex, Spinner } from '@chakra-ui/react';
+import { Container, Flex, Spinner, useToast } from '@chakra-ui/react';
 import useProductById from '../hooks/useProductsById';
-import addToCart from '../hooks/addToCart';
+import addToCart from '../helpers/addToCart';
 import CardDetail from './CardDetail';
 import { AppContext } from '../context/AppContext';
 
@@ -10,12 +10,21 @@ function ProductDetail() {
   const { id } = useParams();
   const { data: product, isLoading } = useProductById(id);
   const { setNumber } = useContext(AppContext);
+  const toast = useToast();
 
   const handleAddToCart = async (productId) => {
     const resp = await addToCart({
       id: productId,
       colorCode: 1,
       storageCode: 2
+    });
+    toast({
+      title: 'Successful',
+      description: 'Your product has been added',
+      status: 'success',
+      duration: 2000,
+      isClosable: true,
+      position: 'top-right'
     });
     const currentCart = localStorage.getItem('cart_count');
     if (currentCart) {
